797c7eea295137ce22416c00a690c9dd584fc126
Faith <kashfaith77@gmail.com>
2025-03-07 17:11:29 +0300

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var customerName="bob"
+function upperCaseCustomerName(){
+     customerName= customerName.toUpperCase()
+    return customerName
+}
+
+
+
+function setBestCustomer(){
+     bestCustomer= "not bob" 
+    
+       
+    }
+
+    function overwriteBestCustomer(){
+        bestCustomer="maybe bob"
+    }
+
+    const LeastFavoriteCustomer="billy"
+    function changeLeastFavoriteCustomer(){
+      LeastFavoriteCustomer="susan"
+     
+    }
+   
+
+
